99f63ea518cde4c5fd75c4f3b35bfcd41234dbc7
Marina Sauca <marina.sauca@gmail.com>
2014-12-04 13:21:40 +0100

    cjtAgents fet

--- src/DominiPKG/AgentPKG/cjtAgents.java
+++ src/DominiPKG/AgentPKG/cjtAgents.java
@@ -1,29 +1,48 @@
 package DominiPKG.AgentPKG;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /**
- * Created by Mirshi on 26/11/14.
+ * @author Marina Sauca
+ * @version 0.1
+ * @since 4.12.2014
  */
 public class cjtAgents {
-    /*
-    TODO
-     */
-    public cjtAgents(){}
+    ArrayList<Agent> agents;
+    public cjtAgents(){
+        agents = new ArrayList<Agent>();
+    }
 
     public void addAgent(int id, String nom) {
+        agents.add(new Agent(id,nom));
     }
 
     public void deleteAgent(int id) {
+        if (!agents.isEmpty()) {
+            Iterator<Agent> it = agents.iterator();
+            while (it.hasNext())
+                if (id == it.next().getId()) {
+                    it.remove();
+                }
+        }
     }
 
-    public Agent getAgents(int id) {
+    public ArrayList<Agent> getAgents() {
+        return agents;
     }
 
     public int getNumAgents() {
-        return 42;
+        return agents.size();
     }
 
     public Agent getAgent(int id) {
         Agent a = new Agent();
+        for (Agent b : agents) {
+            if (a.getId() == id){
+                return b;
+            }
+        }
         return a;
     }
 
