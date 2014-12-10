f9a145cdfb8fad10498b4a2aa5785a34548561e4
Marina Sauca <marina.sauca@gmail.com>
2014-12-10 19:35:22 +0100

    controladorGraf updated

--- src/DominiPKG/ControladorsPKG/ControladorGraf.java
+++ src/DominiPKG/ControladorsPKG/ControladorGraf.java
@@ -1,10 +1,10 @@
 package DominiPKG.ControladorsPKG;
-
 import DominiPKG.GrafPKG.Aresta;
 import DominiPKG.GrafPKG.Graf;
 import DominiPKG.GrafPKG.Vertex;
 
 import java.util.ArrayList;
+
 
 /**
  *
@@ -14,149 +14,225 @@ public class ControladorGraf {
 
     private final Graf G;
 
-    /* Pre: cert
-     * Post: El resultat és un controlador de graf buit
+
+    /** Pre: cert
+     * Post: El resultat és un controlador de graf buit 
      */
+
     public ControladorGraf(){
         G = new Graf();
     }
 
-    /* Pre:
+    /** Pre:
      * Post: Et diu si el vertex amb la id implícita existeix al graf
+     * @param idV
+     * @return
      */
+
     public boolean existeixVertex(int idV){
         return G.existeixVertex(idV);
     }
 
-    /* Pre:
+
+    /** Pre:
      * Post: Et diu si la aresta amb la id implícita existeix al graf
+     * @param idA
+     * @return
      */
+
     public boolean existeixAresta(int idA){
         return G.existeixAresta(idA);
     }
 
-    /* Pre:
+
+    /** Pre: 
      * Post: Afegeix el vertex implícit al graf, si aquest vertex ja hi era al graf, no fa res
+     * @param nom
+     * @return
      */
+
     public int afegirVertex(String nom){
         Vertex v = new Vertex(nom);
         G.afegirVertex(v);
         return v.getId();
     }
 
-    /* Pre:
+
+    /** Pre:
      * Post: Elimina el vertex amb id idV del graf
+     * @param idV
      */
+
     public void eliminarVertex(int idV){
         G.eliminarVertex(idV);
     }
 
-    /* Pre:
+    /** Pre:
      * Post: Afegeix la aresta al graf, si la aresta ja existia no fa res
+     * @param cost
+     * @param capacitat
+     * @param id_vertex_inici
+     * @param id_vertex_final
+     * @return
      */
+
     public int afegirAresta(int cost, int capacitat, int id_vertex_inici, int id_vertex_final){
         Aresta a = new Aresta(cost, capacitat, id_vertex_inici, id_vertex_final);
-        G.getVertex(id_vertex_inici).afegirDesti(a.getId());
-        G.getVertex(id_vertex_final).afegirOrigen(a.getId());
         G.afegirAresta(a);
         return a.getId();
     }
 
-    /* Pre:
+    /** Pre:
      * Post: Elimina l'aresta amb id idA del graf
+     * @param idA
      */
+
     public void eliminarAresta(int idA){
         G.eliminarAresta(idA);
     }
 
+    /**Pre:
+     * Post: Retorna el cost de l'aresta
+     * @param idA
+     * @return
+     */
     public Integer getCostAresta(int idA){
         return G.getAresta(idA).getCost();
     }
 
+    /**Pre:
+     * Post: Retorna la capacitat de l'aresta
+     * @param idA
+     * @return
+     */
     public Integer getCapacitatAresta(int idA){
         return G.getAresta(idA).getCapacitat();
     }
 
+    /**Pre:
+     * Post: canvia el cost a l'aresta idA
+     * @param idA
+     * @param cost
+     */
     public void setCostAresta(int idA, int cost){
         G.getAresta(idA).setCost(cost);
     }
 
+    /** Pre:
+     * Post:canvia la capacitat a l'aresta idA
+     * @param idA
+     * @param capacitat
+     */
     public void setCapacitatAresta(int idA, int capacitat){
         G.getAresta(idA).setCapacitat(capacitat);
     }
 
 
-    /* Pre:
+
+    /** Pre:
      * Post: Retorna el vertex inici del graf
+     * @return
      */
+
     public Vertex getInici(){
         return G.getInici();
     }
 
-    /* Pre:
+
+    /** Pre:
      * Post: El vertex implícit ara és el vertex inici del graf
+     * @param idV
      */
+
     public void setInici(int idV){
         G.setInici(idV);
     }
 
-    /* Pre:
+    /** Pre: 
      * Post: Retorna el vertex fi del graf
+     * @return
      */
+
     public Vertex getFi(){
         return G.getFi();
     }
 
-    /* Pre:
+
+    /** Pre:
      * Post: El vertex implícit ara és el vertex fi del graf
+     * @param idV
      */
+
     public void setFi(int idV){
         G.setFi(idV);
     }
 
-    /* Pre:
+    /** Pre:
      * post: Retorna el numero de vertexs del graf
+     * @return
      */
+
     public int getDimensio(){
         return G.getDimensio();
     }
 
-    /* Pre:
+
+    /** Pre: 
      * Post: Si hi ha algun vertex amb la id implicita al graf, ho retorna, si no, retorna null
+     * @param idV
+     * @return
      */
+
     public Vertex getVertex(int idV){
         return G.getVertex(idV);
     }
 
-    /* Pre:
+    /** Pre:
      * Post: Si els vertexs implicits son correctes, retorna l'aresta que va de idU a idV,
      * si no, retorna null
+     * @param idU
+     * @param idV
+     * @return
      */
+
     public Aresta getAresta(int idU, int idV) {
         return G.getAresta(idU, idV);
     }
 
-    /* Pre:
+    /** Pre:
      * Post: Si la id d'aresta implicita es correcte, retorna l'aresta amb id idA,
      * si no, retorna null
+     * @param idA
+     * @return
      */
+
     public Aresta getAresta(int idA) {
         return G.getAresta(idA);
     }
 
-    /* Pre:
+    /** Pre:
      * Post: Si el vertex existeix, retorna les arestes que apunten a l'implícit,
      * si no, retorna una llista buida
+     * @param idV
+     * @return
      */
+
     public ArrayList<Integer> getAdjacentsOrigen(int idV){
         return G.getAdjacentsOrigen(idV);
     }
 
-    /* Pre:
+    /** Pre:
      * Post: Si el vertex existeix, retorna les arestes als que apunta l'implícit,
      * si no, retorna una llista buida
+     * @param idV
+     * @return
      */
+
     public ArrayList<Integer> getAdjacentsDestins(int idV){
         return G.getAdjacentsDestins(idV);
+    }
+
+    public Graf getGraf(){
+        return G;
     }
 }
